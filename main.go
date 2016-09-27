c8304fedb43207b3dfdb722087d7a1989506d9e2
Neoplatonist <mr.neoplatonist@gmail.com>
2016-09-28 07:18:32 +0900

    Added upload to html and moved upload function into main.go

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	rice "github.com/GeertJohan/go.rice"
 	"github.com/labstack/echo"
@@ -26,4 +29,37 @@ func main() {
 	engine.POST("/upload", upload)
 
 	engine.Run(standard.New(":1337"))
+}
+
+func upload(c echo.Context) error {
+	//-----------
+	// Read file
+	//-----------
+
+	// Source
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create("./pdf/" + file.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	c.HTML(http.StatusOK, fmt.Sprintf("<p>File \"%s\" uploaded successfully.</p><br /><a href=\"/\">Go Back</>", file.Filename))
+
+	return nil
 }
