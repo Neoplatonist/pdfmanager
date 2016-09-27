e5fdaf9a523a26374c7cf5b0ae50dfec448f8e39
Neoplatonist <mr.neoplatonist@gmail.com>
2016-09-28 06:27:30 +0900

    Added http server, go.rice, index.html

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	engine := echo.New()
+
+	engine.SetDebug(true)
+
+	engine.Use(middleware.Logger())
+	engine.Use(middleware.Recover())
+
+	assetHandler := http.FileServer(rice.MustFindBox("pdf").HTTPBox())
+
+	engine.File("/", "index.html")
+
+	engine.GET("/pdf/*", standard.WrapHandler(http.StripPrefix("/pdf/", assetHandler)))
+
+	engine.Run(standard.New(":1337"))
+}
